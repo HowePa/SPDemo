7bdca9b68febef81ec9cba64ce50f69bf66681d2
HowePa <2873679104@qq.com>
2024-02-02 17:08:39 +0800

    update rbtest

--- script/test.sh
+++ script/test.sh
@@ -20,16 +20,14 @@ case "$@" in
         docker exec -it sp-kafka /usr/bin/kafka-topics --bootstrap-server sp-kafka:19092 --list
         ;;
     "create")
-#### create database
         for idx in ${node_id[@]}; do
             create_db="CREATE DATABASE IF NOT EXISTS rbtest;"
             echo "$create_db" | clickhouse-client --host ${node_host[$idx]} --port ${node_port[$idx]} -n
-        done
-
-#### create consumer
-        for idx in ${node_id[@]}; do
-            for ((topic_id=0; topic_id<${num_topics}; topic_id++)); do
-                create_consumer="CREATE TABLE IF NOT EXISTS rbtest.consumer_rb${topic_id}
+            topic_list="rb0"
+            for ((topic_id=1; topic_id<${num_topics}; topic_id++)); do
+                topic_list="${topic_list},rb${topic_id}"
+            done
+            create_sql="CREATE TABLE IF NOT EXISTS rbtest.consumer
 (
     id Int32,
     name String
@@ -37,40 +35,27 @@ case "$@" in
 ENGINE = Kafka()
 SETTINGS 
 kafka_broker_list = 'SASL_PLAINTEXT://sp-kafka:9092', 
-kafka_topic_list = 'rb${topic_id}', 
+kafka_topic_list = '${topic_list}', 
 kafka_format = 'CSV', 
 kafka_row_delimiter = '\n', 
-kafka_group_name = 'rbtest${topic_id}', 
+kafka_group_name = 'rbtest', 
 kafka_client_id = 'node${node_id[$idx]}_rb${topic_id}_r0', 
-kafka_num_consumers = ${num_consumers[$idx]};"
-                echo "$create_consumer" | clickhouse-client --host ${node_host[$idx]} --port ${node_port[$idx]} -n -t
-            done
-        done
+kafka_num_consumers = ${num_consumers[$idx]};
 
-#### create local
-        create_local="CREATE TABLE IF NOT EXISTS rbtest.local_rb ON CLUSTER spcluster
+CREATE TABLE IF NOT EXISTS rbtest.local
 (
     id Int32,
     name String
 )
 ENGINE = MergeTree
 ORDER BY id;
-CREATE TABLE IF NOT EXISTS rbtest.distrib_rb ON CLUSTER spcluster
-(
-    id Int32,
-    name String
-)
-ENGINE = Distributed('spcluster', 'rbtest', 'local_rb', sipHash64(id));"
-        echo "$create_local" | clickhouse-client --host ${node_host[0]} --port ${node_port[0]} -n -t
 
-#### create mv
-        for ((topic_id=0; topic_id<${num_topics}; topic_id++)); do
-            create_mv="CREATE MATERIALIZED VIEW rbtest.mv_rb${topic_id} ON CLUSTER spcluster TO rbtest.distrib_rb AS
+CREATE MATERIALIZED VIEW rbtest.mv TO rbtest.local AS
 SELECT
 id,
 name
-FROM rbtest.consumer_rb${topic_id};"
-            echo "$create_mv" | clickhouse-client --host ${node_host[0]} --port ${node_port[0]} -n -t
+FROM rbtest.consumer;"
+            echo "$create_sql" | clickhouse-client --host ${node_host[$idx]} --port ${node_port[$idx]} -n -t
         done
         ;;
     "drop")
